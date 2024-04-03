ca125d575f0a8e644de6f1f85f80ec812dcb6323
luizbsadev <luiz.bsa.olv@gmail.com>
2024-04-03 12:18:43 -0300

    alterando estruturas de pasta, e criando valores.component

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ValoresComponent } from './valores/valores.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ValoresComponent,
+    
   ],
   imports: [
     BrowserModule,
